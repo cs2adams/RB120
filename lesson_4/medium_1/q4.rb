7bf5aa0e870ce24bc2e04907b7b2586c88610c9c
Cameron Adams <cameronstuartadams@gmail.com>
2023-07-01 09:00:20 -0400

    Medium 1

---
+++ lesson_4/medium_1/q4.rb
@@ -0,0 +1,17 @@
+class Greeting
+  def greet(msg)
+    puts msg
+  end
+end
+
+class Hello < Greeting
+  def hi
+    greet('Hello')
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet('Goodbye')
+  end
+end
